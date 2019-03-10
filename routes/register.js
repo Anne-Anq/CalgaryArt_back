0b7c495da4e8a2b77db50a3d3e69588ad608cbad
Anne Anquetin <anne.anquetin@gmail.com>
2019-03-10 16:51:52 -0600

    add trycatch blocks for venues, users and register routes

--- routes/register.js
+++ routes/register.js
@@ -5,24 +5,28 @@ const config = require('config');
 const baseURL = config.get('baseURL');
 
 router.post('/', imageUpload('avatar'), function (req, res, next) {
+    try {
+        const user = {
+            email: req.body.email,
+            hashed_password: req.body.password,//implement security
+            f_name: req.body.f_name ? req.body.f_name : '',
+            l_name: req.body.l_name ? req.body.l_name : '',
+            avatar_URL: req.file ? `${baseURL}${req.file.filename}` : ''
+        };
 
-    const user = {
-        email: req.body.email,
-        hashed_password: req.body.password,//implement security
-        f_name: req.body.f_name ? req.body.f_name : '',
-        l_name: req.body.l_name ? req.body.l_name : '',
-        avatar_URL: req.file ? `${baseURL}${req.file.filename}` : ''
-    };
 
+        res.locals.connection.query('INSERT INTO users SET ?', user, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
 
-    res.locals.connection.query('INSERT INTO users SET ?', user, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,8 @@ const baseURL = config.get('baseURL');
 //protect passwords
 
 router.get('/', function (req, res, next) {
-    res.locals.connection.query('SELECT * FROM users', function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
-});
-router.get('/:user_id', function (req, res, next) {
-    res.locals.connection.query(`SELECT users.id AS user_id, email, f_name, l_name, avatar_URL, users.created_at, artists.id AS artist_id, bio
-    FROM users 
-    LEFT JOIN artists ON users.id = artists.user_id    
-    WHERE users.id =${req.params.user_id}`, function (error, results, fields) {
+    try {
+        res.locals.connection.query('SELECT * FROM users', function (error, results, fields) {
             if (error) {
                 res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
                 //If there is error, we send the error in the error section with 500 status
@@ -27,38 +15,70 @@ router.get('/:user_id', function (req, res, next) {
                 res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
             }
         });
+    } catch (error) {
+        console.log(error)
+    }
+
+});
+router.get('/:user_id', function (req, res, next) {
+    try {
+        res.locals.connection.query(`SELECT users.id AS user_id, email, f_name, l_name, avatar_URL, users.created_at, artists.id AS artist_id, bio
+    FROM users 
+    LEFT JOIN artists ON users.id = artists.user_id    
+    WHERE users.id =${req.params.user_id}`, function (error, results, fields) {
+                if (error) {
+                    res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                    //If there is error, we send the error in the error section with 500 status
+                } else {
+                    res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+                }
+            });
+    } catch (error) {
+        console.log(error)
+    }
+
 });
 router.put('/:user_id', imageUpload('avatar'), function (req, res, next) {
-    const query = `UPDATE users
+    try {
+        const query = `UPDATE users
     SET  ?
     WHERE id = ?`;
-    const user = {
-        email: req.body.email,
-        hashed_password: req.body.password,//implement security
-        f_name: req.body.f_name ? req.body.f_name : '',
-        l_name: req.body.l_name ? req.body.l_name : '',
-        avatar_URL: req.file ? `${baseURL}${req.file.filename}` : ''
-    };
+        const user = {
+            email: req.body.email,
+            hashed_password: req.body.password,//implement security
+            f_name: req.body.f_name ? req.body.f_name : '',
+            l_name: req.body.l_name ? req.body.l_name : '',
+            avatar_URL: req.file ? `${baseURL}${req.file.filename}` : ''
+        };
 
-    res.locals.connection.query(query, [user, req.params.user_id], function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
+        res.locals.connection.query(query, [user, req.params.user_id], function (error, results, fields) {
+
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
 });
 router.delete('/:user_id', function (req, res, next) {
+    try {
+        res.locals.connection.query('DELETE FROM users WHERE id = ?', req.params.user_id, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
 
-    res.locals.connection.query('DELETE FROM users WHERE id = ?', req.params.user_id, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
 });
 
 module.exports = router;
