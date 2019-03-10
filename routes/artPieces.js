5e185d4a985de913a0e113b77adcf4a3ef93484c
Anne Anquetin <anne.anquetin@gmail.com>
2019-03-10 15:56:16 -0600

    add trycatch blocks for artists and artpieces routes

--- routes/artPieces.js
+++ routes/artPieces.js
@@ -10,23 +10,29 @@ const baseURL = config.get('baseURL');
 
 //ALL ART_PIECES
 router.get('/', function (req, res, next) {
-    const query = `SELECT id, ap_name, ap_picture_URL FROM  art_pieces`
-    res.locals.connection.query(query
-        , function (error, results, fields) {
-            if (error) {
-                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-                //If there is error, we send the error in the error section with 500 status
-            } else {
-                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-            }
-        });
+    try {
+        const query = `SELECT id, ap_name, ap_picture_URL FROM  art_pieces`
+        res.locals.connection.query(query
+            , function (error, results, fields) {
+                if (error) {
+                    res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                    //If there is error, we send the error in the error section with 500 status
+                } else {
+                    res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+                }
+            });
+    } catch (error) {
+        console.log(err)
+    }
+
 });
 
 
 
 //ONE ART_PIECE
 router.get('/:ap_id', function (req, res, next) {
-    const query = `SELECT 
+    try {
+        const query = `SELECT 
     f_name, l_name, ap_name, ap_description, ap_price, ap_picture_URL, artist_id, ap_id, venue_id, 
     b_name, date_from, date_to, exhibitions.id AS exhibition_id, users.avatar_URL
     FROM users 
@@ -35,73 +41,90 @@ router.get('/:ap_id', function (req, res, next) {
     JOIN exhibitions ON art_pieces.id = exhibitions.ap_id
     JOIN venues ON exhibitions.venue_id = venues.id
     WHERE art_pieces.id =${req.params.ap_id};`
-    res.locals.connection.query(query, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
+        res.locals.connection.query(query, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
+
 });
 
 
 //POST ART_PIECE
 router.post('/', imageUpload('ap_picture'), function (req, res, next) {
+    try {
+        const art_piece = {
+            ap_name: req.body.ap_name ? req.body.ap_name : 'unnamed',
+            ap_description: req.body.ap_description ? req.body.ap_description : '',
+            ap_price: req.body.ap_price ? req.body.ap_price : 0,
+            ap_picture_URL: req.file ? `${baseURL}${req.file.filename}` : '',
+            artist_id: req.body.artist_id
+        };
+        res.locals.connection.query('INSERT INTO art_pieces SET ?', art_piece, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
 
-    const art_piece = {
-        ap_name: req.body.ap_name ? req.body.ap_name : 'unnamed',
-        ap_description: req.body.ap_description ? req.body.ap_description : '',
-        ap_price: req.body.ap_price ? req.body.ap_price : 0,
-        ap_picture_URL: req.file ? `${baseURL}${req.file.filename}` : '',
-        artist_id: req.body.artist_id
-    };
-    res.locals.connection.query('INSERT INTO art_pieces SET ?', art_piece, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
 });
 
 
 //UPDATE
 router.put('/:ap_id', imageUpload('ap_picture'), function (req, res, next) {
-    const query = `UPDATE art_pieces
+    try {
+        const query = `UPDATE art_pieces
     SET  ?
     WHERE id = ?`;
-    const art_piece = {
-        ap_name: req.body.ap_name ? req.body.ap_name : 'unnamed',
-        ap_description: req.body.ap_description ? req.body.ap_description : '',
-        ap_price: req.body.ap_price ? req.body.ap_price : 0,
-        ap_picture_URL: req.file ? `${baseURL}${req.file.filename}` : '',
-        artist_id: req.body.artist_id
-    };
+        const art_piece = {
+            ap_name: req.body.ap_name ? req.body.ap_name : 'unnamed',
+            ap_description: req.body.ap_description ? req.body.ap_description : '',
+            ap_price: req.body.ap_price ? req.body.ap_price : 0,
+            ap_picture_URL: req.file ? `${baseURL}${req.file.filename}` : '',
+            artist_id: req.body.artist_id
+        };
 
-    res.locals.connection.query(query, [art_piece, req.params.ap_id], function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
+        res.locals.connection.query(query, [art_piece, req.params.ap_id], function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
+
 });
 
 
 //DELETE
 router.delete('/:ap_id', function (req, res, next) {
+    try {
+        res.locals.connection.query('DELETE FROM art_pieces WHERE id = ?', req.params.ap_id, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
 
-    res.locals.connection.query('DELETE FROM art_pieces WHERE id = ?', req.params.ap_id, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
 });
 
 module.exports = router;
--- routes/artists.js
+++ routes/artists.js
@@ -10,8 +10,35 @@ const baseURL = config.get('baseURL');
 
 //ALL ARTISTS
 router.get('/', function (req, res, next) {
-    res.locals.connection.query('SELECT artists.id, f_name, l_name, avatar_URL FROM artists JOIN users ON artists.user_id = users.id'
-        , function (error, results, fields) {
+    try {
+        res.locals.connection.query('SELECT artists.id, f_name, l_name, avatar_URL FROM artists JOIN users ON artists.user_id = users.id'
+            , function (error, results, fields) {
+                if (error) {
+                    res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                    //If there is error, we send the error in the error section with 500 status
+                } else {
+                    res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+                }
+            });
+    } catch (error) {
+        console.log(error)
+    }
+
+});
+
+
+
+//ONE ARTIST
+router.get('/:artist_id', function (req, res, next) {
+    try {
+        const query = `SELECT 
+    artist_id, ap_name,art_pieces.id AS ap_id, ap_picture_URL, ap_price, ap_description,
+    avatar_URL, f_name, l_name, bio
+    FROM users 
+    LEFT JOIN artists ON users.id = artists.user_id 
+    LEFT JOIN art_pieces ON artists.id = art_pieces.artist_id
+    WHERE artists.id =${req.params.artist_id};`
+        res.locals.connection.query(query, function (error, results, fields) {
             if (error) {
                 res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
                 //If there is error, we send the error in the error section with 500 status
@@ -19,81 +46,78 @@ router.get('/', function (req, res, next) {
                 res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
             }
         });
-});
+    } catch (error) {
+        console.log(err)
+    }
 
-
-
-//ONE ARTIST
-router.get('/:artist_id', function (req, res, next) {
-    const query = `SELECT 
-    artist_id, ap_name,art_pieces.id AS ap_id, ap_picture_URL, ap_price, ap_description,
-    avatar_URL, f_name, l_name, bio
-    FROM users 
-    LEFT JOIN artists ON users.id = artists.user_id 
-    LEFT JOIN art_pieces ON artists.id = art_pieces.artist_id
-    WHERE artists.id =${req.params.artist_id};`
-    res.locals.connection.query(query, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
 });
 
 
 
 // //Add an artist from user id
 router.post('/', function (req, res, next) {
-    const artist = {
-        user_id: req.body.user_id,
-        bio: req.body.bio ? req.body.bio : ''
-    };
-    res.locals.connection.query('INSERT INTO artists SET ?', artist, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
+    try {
+        const artist = {
+            user_id: req.body.user_id,
+            bio: req.body.bio ? req.body.bio : ''
+        };
+        res.locals.connection.query('INSERT INTO artists SET ?', artist, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
+
 });
 
 
 
 //UPDATE ARTIST
 router.put('/:artist_id', imageUpload('avatar'), function (req, res, next) {
-    const query = `UPDATE artists
+    try {
+        const query = `UPDATE artists
     SET  ?
     WHERE id = ?`;
-    const artist = {
-        user_id: req.body.user_id,
-        bio: req.body.bio ? req.body.bio : ''
-    };
-    res.locals.connection.query(query, [artist, req.params.artist_id], function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
+        const artist = {
+            user_id: req.body.user_id,
+            bio: req.body.bio ? req.body.bio : ''
+        };
+        res.locals.connection.query(query, [artist, req.params.artist_id], function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
+
 });
 
 
 
 //DELETE ARTIST
 router.delete('/:artist_id', function (req, res, next) {
+    try {
+        res.locals.connection.query('DELETE FROM artists WHERE id = ?', req.params.artist_id, function (error, results, fields) {
+            if (error) {
+                res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+            }
+        });
+    } catch (error) {
+        console.log(err)
+    }
 
-    res.locals.connection.query('DELETE FROM artists WHERE id = ?', req.params.artist_id, function (error, results, fields) {
-        if (error) {
-            res.send(JSON.stringify({ "status": 500, "error": error, "response": null }));
-            //If there is error, we send the error in the error section with 500 status
-        } else {
-            res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
-        }
-    });
 });
 
 
